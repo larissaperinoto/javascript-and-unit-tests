cf504582c18cdd087297400408a46d8d6c9d8a0e
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-11 19:51:33 -0300

    [Larissa Perinoto] - Project js unit test [Requisito 2]

--- src/average.js
+++ src/average.js
@@ -11,21 +11,27 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
+const aux = (array) => {
+  let counter = 0;
+  for (let item of array) {
+    if (typeof item !== 'number') {
+      return undefined;
+    } 
+    counter += item;
+  }
+  return counter;
+};
 
 const average = (array) => {
   let counter = 0;
-  if (array.length !== 0) {
-    for (let item of array) {
-      if (typeof item !== 'number') {
-        return undefined;
-      } 
-      counter += item;
-    }
+  if (array.length !== 0 && aux(array) !== undefined) {
+   counter = aux(array);
   } else {
     return undefined;
   }
-
   return Math.round(counter / array.length);
 };
+
+console.log(average([3, 4, 5]));
 
 module.exports = average;
