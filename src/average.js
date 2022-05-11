1e280c88d7f4467a8926180fec7fb6d7671a29e0
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-11 16:52:31 -0300

    [Larissa Perinoto] Iniciando o projeto! #VQV

--- src/average.js
+++ src/average.js
@@ -4,7 +4,7 @@
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
 
   Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
+    -   array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
     - average([2, 2]) // Retorno: 2;
     - average([1, 1]) // Retorno: 1;
