e9a47889dd43460828b5577b96ad744d2cfeb8a1
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-12 20:00:54 -0300

    [Larissa Perinoto] - Project js unit tests [Requisito 8]

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,15 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter < 4; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let count = 2; count < 4; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
