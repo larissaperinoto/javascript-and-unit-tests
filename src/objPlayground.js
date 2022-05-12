b50b059641c50476e750892ac35bd98d5c3c43cb
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-12 19:22:27 -0300

    [Larissa Perinoto] - Project js unit tests [Requisito 7]

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,19 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const obj = {
+    sum: Math.floor(number1 + number2),
+    mult: Math.floor(number1 * number2),
+    div: Math.floor(number1 / number2),
+    sub: Math.floor(number1 - number2),
+  };
+  return obj;
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  const tipo = type;
+ return Object[type](object);
+};
 
 module.exports = { calculator, arrayGenerator };
