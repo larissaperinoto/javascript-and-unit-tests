d39662d1a0ae6630cac8588d2dafabd832f99ea6
Larissa Perinoto <perinotolarissa@gmail.com>
2022-12-13 08:53:02 -0300

    Remove comentarios.

--- src/average.js
+++ src/average.js
@@ -1,22 +1,9 @@
-/*
-  A função average recebe um array de tamanho variável e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não numérico ou um array vazio, o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
-  Parâmetros:
-    -   array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
-  Comportamento:
-    - average([2, 2]) // Retorno: 2;
-    - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined;
-*/
-
 const aux = (array) => {
   let counter = 0;
   for (let item of array) {
     if (typeof item !== 'number') {
       return undefined;
-    } 
+    }
     counter += item;
   }
   return counter;
--- src/productDetails.js
+++ src/productDetails.js
@@ -1,32 +1,3 @@
-/*
-  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara')
-
-  // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  Escreva pelo menos cinco testes para essa função no arquivo tests/productDetails.js para garantir que a implementação de productDetails está correta.
-*/
-
 const productDetails = (firstProduct, secondProduct) => [
   {
     name: firstProduct,
